15f5a236df78a3a478ca70810e2733fca00412a4
Keegan Neave <keegan.neave@outlook.com>
2021-01-11 18:23:07 +0000

    switched to joystick64

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <Encoder.h>
 
+/*
+  BIG NOTE!
+
+    This wont work unless you change JOYSTICK_SIZE to 64 in usb_desc.h
+*/
+
 /* Complete USB Joystick Example
    Teensy becomes a USB joystick with 16 or 32 buttons and 6 axis input
 
@@ -18,10 +24,15 @@
 // the analog voltage.
 
 const int numButtons = 6;  // 16 for Teensy, 32 for Teensy++
-const int buttonStart = 2;  // 16 for Teensy, 32 for Teensy++
+const int buttonStart = 2; // 16 for Teensy, 32 for Teensy++
 const int joy1button = 23;
 const int joy2button = 16;
 const int encButton = 10;
+
+const int deadzone = 3000;
+const int midPoint = 32767;
+const int lowPoint =  midPoint - deadzone;
+const int highPoint = midPoint + deadzone;
 
 Encoder enc(12, 11);
 
@@ -33,14 +44,16 @@ int state = -1;
 unsigned long lastChange = 0;
 unsigned long changeInterval = 1000;
 
-void setup() {
+void setup()
+{
   // you can print to the serial monitor while the joystick is active!
   Serial.begin(9600);
   // configure the joystick to manual send mode.  This gives precise
   // control over when the computer receives updates, but it does
   // require you to manually call Joystick.send_now().
   Joystick.useManualSend(true);
-  for (int i=buttonStart; i< buttonStart + numButtons; i++) {
+  for (int i = buttonStart; i < buttonStart + numButtons; i++)
+  {
     pinMode(i, INPUT_PULLUP);
   }
   pinMode(joy1button, INPUT_PULLUP);
@@ -55,6 +68,8 @@ void setup() {
   analogWrite(blueLed, 255);
   analogWrite(greenLed, 255);
 
+  enc.write(512);
+
   Serial.println("Begin Complete Joystick Test");
 }
 
@@ -63,22 +78,80 @@ byte prevButtons[numButtons];
 
 volatile long angle = 0;
 
-void loop() {
+int CheckAxisValue(int value, bool invert)
+{
+  if (invert)
+  {
+    value = map(value, 0, 1023, 65535, 0);
+  }
+  else
+  {
+    value = map(value, 0, 1023, 0, 65535);
+  }
+  
+
+  if ((value > lowPoint) & (value < highPoint))
+  {
+    value = midPoint;
+  }
+
+  return value;
+}
+
+void UpdateLED()
+{
+  switch (state)
+  {
+  case 0:
+    analogWrite(redLed, 127);
+    analogWrite(blueLed, 255);
+    analogWrite(greenLed, 255);
+    break;
+  case 1:
+    analogWrite(redLed, 255);
+    analogWrite(blueLed, 127);
+    analogWrite(greenLed, 255);
+    break;
+  case 2:
+    analogWrite(redLed, 255);
+    analogWrite(blueLed, 255);
+    analogWrite(greenLed, 127);
+    break;
+  }
+
+  unsigned long timeDiff = millis() - lastChange;
+  if (timeDiff >= changeInterval)
+  {
+    state++;
+    if (state > 2)
+    {
+      state = 0;
+    }
+    lastChange = millis();
+  }
+}
+
+void loop()
+{
   // read 6 analog inputs and use them for the joystick axis
-  Joystick.X(map(analogRead(20), 0, 1023, 1023, 0));
-  Joystick.Y(analogRead(21));
-  Joystick.Z(analogRead(22));
-  Joystick.Zrotate(map(analogRead(19), 0, 1023, 1023, 0));
-  Joystick.sliderLeft(analogRead(17));
-  Joystick.sliderRight(map(analogRead(18), 0, 1023, 1023, 0));
+  Joystick.X(CheckAxisValue(analogRead(20), true));
+  Joystick.Y(CheckAxisValue(analogRead(21), true));
+  Joystick.Z(CheckAxisValue(analogRead(22), false));
+  Joystick.Xrotate(CheckAxisValue(analogRead(19), true));
+  Joystick.Yrotate(CheckAxisValue(analogRead(18), true));
+  Joystick.Zrotate(CheckAxisValue(analogRead(17), false));
   
   // read digital pins and use them for the buttons
-  for (int i=0; i < numButtons; i++) {
-    if (digitalRead(i+buttonStart)) {
+  for (int i = 0; i < numButtons; i++)
+  {
+    if (digitalRead(i + buttonStart))
+    {
       // when a pin reads high, the button is not pressed
       // the pullup resistor creates the "on" signal
       allButtons[i] = 0;
-    } else {
+    }
+    else
+    {
       // when a pin reads low, the button is connecting to ground.
       allButtons[i] = 1;
     }
@@ -102,12 +175,13 @@ void loop() {
 
   // make the hat switch automatically move in a circle
   long newAngle = enc.read();
-  if(newAngle > 315) 
-  { 
-    enc.write(315);
-    angle = 315;
+  
+  if (newAngle > 1024)
+  {
+    enc.write(1024);
+    angle = 1024;
   }
-  else if(newAngle < 0)
+  else if (newAngle < 0)
   {
     enc.write(0);
     angle = 0;
@@ -115,66 +189,37 @@ void loop() {
   else
   {
     angle = newAngle;
-  }  
-  
-  Joystick.hat(angle);
-  
+  }
+
+  Joystick.slider(1, angle * 64);
+
   // Because setup configured the Joystick manual send,
   // the computer does not see any of the changes yet.
   // This send_now() transmits everything all at once.
   Joystick.send_now();
-  
+
   // check to see if any button changed since last time
   boolean anyChange = false;
-  for (int i=0; i<numButtons; i++) {
-    if (allButtons[i] != prevButtons[i]) anyChange = true;
+  for (int i = 0; i < numButtons; i++)
+  {
+    if (allButtons[i] != prevButtons[i])
+      anyChange = true;
     prevButtons[i] = allButtons[i];
   }
-  
+
   // if any button changed, print them to the serial monitor
-  if (anyChange) {
+  if (anyChange)
+  {
     Serial.print("Buttons: ");
-    for (int i=0; i<numButtons; i++) {
+    for (int i = 0; i < numButtons; i++)
+    {
       Serial.print(allButtons[i], DEC);
     }
     Serial.println();
   }
 
-
+  UpdateLED();
 
   // a brief delay, so this runs "only" 200 times per second
   delay(5);
-}
-
-void UpdateLED()
-{
-    switch(state)
-  {
-    case 0:
-      analogWrite(redLed, 127);
-      analogWrite(blueLed, 255);
-      analogWrite(greenLed, 255);
-      break;
-    case 1:
-      analogWrite(redLed, 255);
-      analogWrite(blueLed, 127);
-      analogWrite(greenLed, 255);
-      break;
-    case 2:
-      analogWrite(redLed, 255);
-      analogWrite(blueLed, 255);
-      analogWrite(greenLed, 127);
-      break;
-  }
-  
-  unsigned long timeDiff = millis() - lastChange;
-  if(timeDiff >= changeInterval)
-  {
-    state++;
-    if(state > 2)
-    {
-      state = 0;
-    }
-    lastChange = millis();
-  }
 }
